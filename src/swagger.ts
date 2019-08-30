659721f8793825e853528c1816d80c718935b308
louzoid <Louise@louises-mbp.home>
2019-08-30 12:30:08 +0100

    Problems hit with the swagger templates.

--- src/swagger.ts
+++ src/swagger.ts
@@ -9,5 +9,6 @@ export function swaggerInit(app) {
         .build();
     const document = SwaggerModule.createDocument(app, options);
     SwaggerModule.setup('api', app, document);
-    
+    //stuck again. All working locally but deploying to serverless via the optimize
+    //plugin causes the templates to be in an unexpected place. Bah!
 }
